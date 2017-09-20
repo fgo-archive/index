c7adcd7c0dae0da378fe78a44e8126d82e028549
0v0 <0v0@xx.com>
2017-09-20 16:50:40 +0800

    修正童谣错误的羁绊礼装ID

--- js/title-link.js
+++ js/title-link.js
@@ -1,14 +1,15 @@
 $(document).ready(function () {
 	var pages = {
 		' ' : '首页',
+		'index.html' : '首页',
 		'fcraft.html' : '羁绊礼装表',
 		'fcraft.html?no_pic' : '羁绊礼装表(无图版)',
 		'np_cal.html' : 'NP计算器',
 		'exp_cal.html' : '狗粮计算器',
-		'drop_chance.html':'效率剧场'
+		'drop_chance.html' : '效率剧场'
 	};
 	var page = window.location.pathname + window.location.search;
-	if (page.split('/')[2]) {
+	if (page.split('/')[2] && page.split('/')[2] != 'index.html') {
 		page = page.split('/')[2];
 		for (var i in pages) {
 			if (page == i) {
@@ -18,7 +19,7 @@ $(document).ready(function () {
 		}
 	} else {
 		for (var i in pages) {
-			if (i == ' ') {
+			if (i == ' ' || i == 'index.html') {
 				continue;
 			}
 			$("#contents").append('<h3><a href="' + i + '">' + pages[i] + '</a></h3>');
