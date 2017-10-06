753aac4bb16c824bd5b34a241c9739d6f8e92be5
0v0 <0v0@xx.com>
2017-10-06 17:49:50 +0800

    羁绊礼装进度更新

--- js/title-link.js
+++ js/title-link.js
@@ -1,18 +1,18 @@
 $(document).ready(function () {
 	var pages = {
-		' ' : '首页',
-		'index.html' : '首页',
-		'fcraft.html' : '羁绊礼装表',
-		'fcraft.html?no_pic' : '羁绊礼装表(无图版)',
-		'np_cal.html' : 'NP计算器',
-		'exp_cal.html' : '狗粮计算器',
-		'drop_chance.html' : '效率剧场'
+		' ': '首页',
+		'index.html': '首页',
+		'fcraft.html': '羁绊礼装表',
+		'fcraft.html?no_pic': '羁绊礼装表(无图版)',
+		'np_cal.html': 'NP计算器',
+		'exp_cal.html': '狗粮计算器',
+		'drop_chance.html': '效率剧场'
 	};
 	var page = window.location.pathname + window.location.search;
 	if (page.split('/')[2] && page.split('/')[2] != 'index.html') {
 		page = page.split('/')[2];
 		for (var i in pages) {
-			if (page == i) {
+			if (page == i || i == 'index.html') {
 				continue;
 			}
 			$("#title_link").append('<a href="' + './' + i + '">' + pages[i] + '</a>&nbsp;&nbsp;');
